b2069fa51c85af65e609f2ea8535690a31ceeb94
joascode <joaskramer@outlook.com>
2018-03-29 12:58:31 +0200

    Added start of the LoadBalancer in WPF.

---
+++ LoadBalancer/LoadBalancer/LoadBalancer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadBalancer
+{
+    class LoadBalancer
+    {
+        Dictionary<int, Server> servers = new Dictionary<int, Server>();
+        Dictionary<int, int> sessions = new Dictionary<int, int>();
+
+        TcpListener tcpListener;
+
+        public LoadBalancer()
+        {
+
+        }
+
+        public void Listen()
+        {
+            //Start listener.
+
+            //Check on TcpClientsAcceptance > server or client.
+
+            //Pass client to Server for handling messages and callback to forward response to client.
+        }
+
+    }
+}
---
+++ LoadBalancer/LoadBalancer/Server.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadBalancer
+{
+    class Server
+    {
+        Dictionary<int, Client> clients = new Dictionary<int, Client>();
+    }
+}
